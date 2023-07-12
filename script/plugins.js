bb2efeb0d3d8c74ae06bbfb4b88486f5d4e8b43b
NoobHamada <100050458+NoobHamada@users.noreply.github.com>
2023-07-12 19:08:09 +0300

    Add files via upload

--- script/plugins.js
+++ script/plugins.js
@@ -1,19 +1,38 @@
 $(document).ready(function () {
-    $("aside").click(function () {
+    $("aside").on('click', (function () {
         $(this).children("span").toggle();
+        $("#ttl").toggleClass("pushed");
         $("#cart").fadeToggle(300);
-        $("#ttl").toggleClass("pushed")
-    })
-    
-    
-    $("button").click(function () {
+
+    }))
+
+
+    $("button").on('click', (function () {
         $(this).children().animate({
-            top:'4px',
-            left:'0',
-            height:'20px',
-            width:'20px',
+            top: '4px',
+            left: '0',
+            height: '20px',
+            width: '20px',
         }, 200).fadeOut(2000).css({
             backgroundColor: 'purple'
         })
+    }))
+
+
+    $("select").on('change', function () {
+        switch ($(this).val()) {
+            case 'Fruit':
+                $(".vegetable").hide();
+                $(".fruit").show();
+                break;
+            case 'Vegetable':
+                $(".vegetable").show();
+                $(".fruit").hide();
+                break;
+            case 'All':
+                $(".vegetable").show();
+                $(".fruit").show();
+                break;
+        }
     })
 })
