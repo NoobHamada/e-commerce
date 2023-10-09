cbced9c2e7437339718950cdec189618fc873840
NoobHamada <100050458+NoobHamada@users.noreply.github.com>
2023-10-09 15:00:41 +0300

    Add files via upload

--- script/main.js
+++ script/main.js
@@ -1,32 +1,60 @@
-let buttons = document.querySelectorAll("button");
-let temp = '';
-let priceArr = [];
-let ttl = document.getElementById("ttl");
+// Getting JSON Data
+let request = new XMLHttpRequest()
+request.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+        let object = JSON.parse(this.responseText)
 
-buttons.forEach(function(ele) {
-    ele.addEventListener('click', function() {
-        priceArr.push(+this.previousElementSibling.innerText)
-        temp += this.previousElementSibling.previousElementSibling.innerText
-        listFill();
-        
-        let ball = document.createElement("div");
-        ball.classList = 'ball';
-        this.appendChild(ball);
-        price();
+        for (i = 0; i < object.length; i++) {
+            let name = document.createElement('div')
+            name.innerText = object[i].name
+            let image = document.createElement('img')
+            image.setAttribute('src', object[i].img)
+            let staticPrice = document.createElement('span')
+            staticPrice.innerText = object[i].price
+            let click = document.createElement('button')
+            click.innerText = '+'
+
+            document.getElementById('section').appendChild(name)
+            document.getElementById('section').appendChild(image)
+            document.getElementById('section').appendChild(staticPrice)
+            document.getElementById('section').appendChild(click)
+        }
+    }
+
+    // The Rest Of The Code
+    let buttons = document.querySelectorAll("button");
+    let temp = '';
+    let priceArr = [];
+    let ttl = document.getElementById("ttl");
+
+    buttons.forEach(function (ele) {
+        ele.addEventListener('click', function () {
+            priceArr.push(+this.previousElementSibling.innerText)
+            temp += this.previousElementSibling.previousElementSibling.previousElementSibling.innerText
+            
+            listFill();
+
+            let ball = document.createElement("div");
+            ball.classList = 'ball';
+            this.appendChild(ball);
+            price();
+        })
     })
-})
 
-function listFill() {
-    let li = document.createElement("li");
-    li.innerText = temp;
-    temp = '';
-    document.getElementById("cart").appendChild(li)
-}
+    function listFill() {
+        let li = document.createElement("li");
+        li.innerText = temp;
+        temp = '';
+        document.getElementById("cart").appendChild(li)
+    }
 
-function price() {
-    let calc = priceArr.reduce(function(a, b) {
-        return a + b
-    })
-    
-    ttl.innerText = calc;
+    function price() {
+        let calc = priceArr.reduce(function (a, b) {
+            return a + b
+        })
+
+        ttl.innerText = calc;
+    }
 }
+request.open('get', 'script/data.json', true);
+request.send()
